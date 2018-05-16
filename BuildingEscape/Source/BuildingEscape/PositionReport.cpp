73680879d583590574b8b4d73e53ebb6399dcd45
laen myth <cyberraptor@hotmail.com>
2018-05-16 16:30:52 +1000

    Accessing_Object_Names_S03_L66

--- BuildingEscape/Source/BuildingEscape/PositionReport.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReport.h"
-
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UPositionReport::UPositionReport()
@@ -10,7 +10,7 @@ UPositionReport::UPositionReport()
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
+	
 }
 
 
@@ -19,10 +19,48 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	FString ObjectName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty on %s!"), *ObjectName);
 }
 
+/*
+struct Record
+{
+	String name;
+	int age;
+};
+
+void foo()
+{
+	Record r;
+	r.name = "Dom";
+	r.age = 100;
+
+	Object * pObject = nullptr;
+	//...
+	pObject = &o;
+
+	UE_LOG(LogTemp, Warning, TEXT("%s%x\n"), "This is a pointer! ", pObject); // --> "0x8A7B"
+
+
+
+
+	int a = 5;
+
+	int * pA = &a;
+	char *
+	Object *
+
+
+	int * p = nullptr;
+
+	if (p)
+		*p = 4;
+
+
+
+}
+*/
 
 // Called every frame
 void UPositionReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
